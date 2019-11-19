d92c4a34786e7619ee6759b95e63a5f8fe87bbd1
Tyler Dolph <memespaghettiofficial@gmail.com>
2019-11-18 21:28:31 -0600

    flip, hellfire, and some basic image link detection
    ye

--- aespir.py
+++ aespir.py
@@ -8,16 +8,20 @@ client = commands.Bot(command_prefix = '-')
 async def on_ready():
     print('aespir is ready')
 
+
+
 client.remove_command('help')
 @client.command()
 async def help(ctx): #a custom yet garbage help command
-    await ctx.send('*aespir v.0*\n**commands:**\nping\n8ball {your question}\nmeme\naddmeme {a link to your meme}\n')
+    await ctx.send('```aespir v.0 - prefix \'-\'\n---\ncommands:\nping\nflip\n8ball {your question}\nmeme\nmemeadd {a link to your meme}```')
     print(f'help     {round(client.latency*1000)}ms')
+
 
 @client.command()
 async def ping(ping): #pong!
     await ping.send(f'pong! {round(client.latency*1000)}ms')
     print(f'pong     {round(client.latency*1000)}ms')
+
 
 @client.command(aliases =['8ball']) #an 8ball command idk
 async def _8ball(ctx,*,question):
@@ -25,13 +29,19 @@ async def _8ball(ctx,*,question):
                   'As I see it, yes.','Most likely.','Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.',
                   'Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.',
                   'Don\'t count on it.','My reply is no.','My sources say no.','Outlook not so good.','Very doubtful.']
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+    await ctx.send(f'```Question: {question}\nAnswer: {random.choice(responses)}```')
     print(f'8ball    {round(client.latency*1000)}ms')
+
+@client.command()
+async def flip(ctx):
+    coin = ['heads','tails']
+    await ctx.send(f'```you flipped {random.choice(coin)}!```')
+    print(f'flip     {round(client.latency*1000)}ms')
 
 @client.command()
 async def meme(ctx): #memes yeyeye
      memes = open("memes.txt","r")
-     oof = "AAAAH"
+     oof = "shrek is hot"
      memeslist = []
      while oof != '':
          oof = raw(str(memes.readline()))
@@ -43,16 +53,50 @@ async def meme(ctx): #memes yeyeye
      memes.close()
      print(f'meme     {round(client.latency*1000)}ms')
 
+
+filestuff = ['.gif','.png','.jpg','.mov','.mp4','.mp3']
 @client.command() #command to add  m e m e s
-async def addmeme(ctx,*,link):
+async def memeadd(ctx,*,link):
+    islink = False
+    bruh = False
+    for end in filestuff:
+        if end in link: islink = True
     memes = open("memes.txt","a")
-    memes.write('\n'+link)
-    await ctx.send('your meme has been added to the collection :)')
+    for aaa in escape_dict:
+        if aaa in link: bruh = True
+    if islink and not bruh:
+        memes.write('\n'+link)
+        await ctx.send('your meme has been added to the collection :)')
+    else:
+        await ctx.send('woah there buckaroo, not so fast. we only want meme links in these parts, \'yahear.')
+    memes.close()
     print(f'addmeme  {round(client.latency*1000)}ms')
 
-    
-escape_dict={'':r'','\b':r'','\c':r'','\f':r'','\n':r'','\r':r'','\t':r'','\v':r'','\'':r'','\"':r'','\0':r'','\1':r'','\2':r'',
+
+
+escape_dict={'\b':r'','\c':r'','\f':r'','\n':r'','\r':r'','\t':r'','\v':r'',
+             '\'':r'','\"':r'','\0':r'','\1':r'','\2':r'',
              '\3':r'','\4':r'','\5':r'','\6':r'','\7':r'','\8':r'','\9':r''}
+
+@client.command()
+async def hellfire(ctx,*,num = '10'):
+    await ctx.send('awaiting administrator approval...')
+    ohfrick = 'sans'
+    while ohfrick != 'n' and ohfrick != 'y':
+        ohfrick = input('hellfire initiated, '+num+'. proceed? (y/n) ')
+    if ohfrick == 'y':
+        await ctx.send('hellfire approved. commencing...')
+        print('hellfire approved, commencing...')
+        i = 0
+        while i < int(num):
+            await ctx.send('@everyone')
+            print(f'hellfire {round(client.latency*1000)}ms')
+            i += 1
+    else:
+        await ctx.send('hellfire denied.')
+        print('hellfire denied.')
+            
+
 
 def raw(text):
     #gets rid of unfresh and unrad characters that we don't want
@@ -65,5 +109,17 @@ def raw(text):
 
 tokenfile = open("token.txt","r") #get the token from token.txt
 token = raw(str(tokenfile.readline()))
-print('readying with token '+token)
-client.run(token)
+tokenfile.close()
+def clientrun():
+    global token
+    print('readying with token '+token)
+    try:
+        client.run(token)
+    except Exception:
+        newtoken = input('error, bad token passed. new token-\n')
+        tokenfile = open("token.txt","a")
+        tokenfile.truncate(0)
+        tokenfile.write(newtoken)
+        tokenfile.close()
+        input('sorry, but aespir needs to take a little nap. please run the file again.\n(press enter to exit)')
+clientrun()
