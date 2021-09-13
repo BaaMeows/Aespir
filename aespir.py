7b2ff8a4ad5bb77de1a49db07667c019de8001b3
Tyler Dolph <tdolph0016@myduneland.org>
2021-09-13 16:45:06 -0600

    added quote command

--- aespir.py
+++ aespir.py
@@ -1,8 +1,10 @@
 #Tyler Dolph 2019-2021
 #=======================================#
+from asyncio.windows_events import NULL
 import discord
 from discord import FFmpegPCMAudio
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 import random
 import time
 import os, os.path
@@ -13,6 +15,9 @@ import psutil
 import socket
 import json
 import sys
+from os import system, name
+import datetime
+import string
 #=======================================# from data.json
 with open('data.json') as file: data = json.load(file)
 PREFIX = data['prefix']
@@ -21,9 +26,10 @@ TOKEN = data['token']
 with open('dadList.json') as file: nodadlist = json.load(file)
 #=======================================# funky variables
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-filestuff = ['.gif','.png','.jpg','.mov','.mp4','.mp3','.webp','.webP','.jpeg','.webm']
+filestuff = ['gif','png','jpg','mov','mp4','mp3','webp','webP','jpeg','webm']
 start=time.time()
 client = commands.Bot(command_prefix = PREFIX)
+channel = NULL #channel that's being watched in the terminal
 #=======================================# counters
 totalCommands = 0
 #=======================================# yeehaw
@@ -40,6 +46,11 @@ async def on_message(message):
     id = message.channel.id
     if message.author == client.user: return
     msg = message.content.lower()
+    if(channel and id==channel.id):
+        links = ""
+        for attachment in message.attachments:
+            links += " ["+attachment.url+"]"
+        print("[" + message.author.name + "] " + message.content + links)
 #=======================================# dad
     if(id not in nodadlist):
         dadmsg = msg.replace(",","")
@@ -58,6 +69,7 @@ async def on_message(message):
         await message.add_reaction(emoji)
         await cmdlog(emoji)
 @client.command()
+@has_permissions(administrator=True)
 async def goawaydad(ctx):
     id = ctx.channel.id
     if id in nodadlist:
@@ -70,6 +82,7 @@ async def goawaydad(ctx):
     await cmdlog('dadaway')
 #=======================================#
 @client.command()
+@has_permissions(administrator=True)
 async def comebackdad(ctx):
     id = ctx.channel.id
     if id not in nodadlist:
@@ -87,9 +100,9 @@ async def dumpJson(filename, data):
 client.remove_command('help')
 @client.command()
 async def help(ctx):
-    await ctx.send('''***#=======================================#***
-Aespir v0.5.2, spagoogi#5559 2019-2021\nprefix: '''+PREFIX+'''\n
-***--- recreational discordbottery***
+
+    embed=discord.Embed(title="Aespir v0.5.3, spagoogi#5559 2019-2021, prefix: "+PREFIX, url="https://discord.com/oauth2/authorize?client_id=459165488572792832&scope=bot",
+    description='''***--- recreational discordbottery***
 flip (a coin)
 8ball {your question}
 echo {your message}
@@ -98,34 +111,45 @@ pop {custom message, defaults to pop} (hehe)
 gay {message (optional)} (gay gay homosexual gay, yay!)
 pingme (pings you after a randomized timer. why would you use this?????)
 
+***--- media commands***
+meme (yes)
+addmeme {media attachment} (more memes!!!)
+cute (absolutely)
+addcute {media attachment} (more puppies!!!)
+
 ***--- bot health and other boring tidbits***
 ping (pong!)
 stats (for nerds!)
-goawaydad (removes dad jokes)
-comebackdad (brings back dad jokes)
 help (you're using it right now!!)
-
-***--- media commands***
-meme (yes)
-addmeme {media attachment or link} (more memes!!!)
-cute (absolutely)
-addcute {media attachment or link} (more puppies!!!)
 
 ***--- NSFW channel only***
 roulette (russian!)
 roulettespin (spins the chamber, if you're into that sort of thing)
 roulettebutwithasemiautomaticpistol (not a good idea)
 
+***--- admin only***
+goawaydad (removes dad jokes, per channel)
+comebackdad (brings back dad jokes, per channel)
+
 ***--- :D***
 invite (yes please)
 sourcecode (goodie!)
-***#=======================================#***''')
+''',
+    color=0xaad5d3)
+    await ctx.send(embed=embed)
     await cmdlog('help')
 #=======================================# pong!
 @client.command()
 async def ping(ping):
     await ping.send(f'pong! {round(client.latency*1000)}ms')
     await cmdlog('pong!')
+#=======================================# yes!!!!!
+@client.command()
+async def pet(ctx):
+    data['pets'] += 1
+    with open("data.json", "w") as jsonFile: json.dump(data, jsonFile)
+    await ctx.send(f"thanks!!!!\nI have been pet a total of {data['pets']} times")
+    await cmdlog('pet')
 #=======================================# fancy
 #uptime: {time.strftime("%H:%M:%S", psutil.boot_time())}
 @client.command(pass_context=True)
@@ -173,6 +197,13 @@ async def uwu(ctx,*,text):
 async def echo(ctx,*,text):
     await ctx.send(text)
     await cmdlog('echo')
+#=======================================# 
+@client.command() 
+async def tylersimulator(ctx):
+    s = ''.join(random.choice(string.ascii_letters) for i in range(random.randint(7,16)))
+    arr = [ 'boobie' , 'balls owo', 'whhat??', 'ghhbbb','minecraf c:', 'based', 'the yoinky sploinky', 'beepbop skeebo!', s,s,s,s ]
+    await ctx.send(random.choice(arr))
+    await cmdlog('tyler')
 #=======================================# why would you do this
 @client.command(pass_context=True) 
 async def pingme(ctx):
@@ -262,11 +293,15 @@ async def image(ctx, sent, folder, message):
 async def addimage(ctx, link, folder):
     global user_agent 
     headers={'User-Agent':user_agent,}
-    if ctx.message.attachments: link = ctx.message.attachments[0].url
-    islink = False
-    if link:
-        for end in filestuff:
-            if end in link[len(link)-len(end):len(link)]: islink = True
+    isLink = False
+    if ctx.message.attachments:
+        link = ctx.message.attachments[0].url
+        islink = True
+    #elif link and "." in link:
+    #    islink = False
+    #    fileType = link.split(".")[-1].lower()
+    #    for end in filestuff:
+    #        if end == fileType: islink = True
     if islink:
         request=urllib.request.Request(link, None, headers)
         response = urllib.request.urlopen(request)
@@ -329,7 +364,7 @@ async def gay(ctx,*,userString = None):
     random.seed(userString+userString)
     num = int(random.random()*101)
     if num > 95: num = 100
-    if num < 5: num = 0
+    if num < 1: num = 1
     if userString == ctx.message.author.mention: await ctx.send('you are '+ str(num) +'%'+ ' gay')
     else: await ctx.send(userString + ' is ' + str(num) +'%'+ ' gay')
     await cmdlog('gay')
@@ -348,6 +383,67 @@ async def join(ctx):
     #voice.play(discord.FFmpegPCMAudio('RobotRock.mp3'), after=lambda e:  print('done', e))
     #voice.disconnect()
     await cmdlog('join')
+#=======================================#
+@client.command()
+async def quoteme(ctx,*,text = ""):
+    rawmessages = await ctx.channel.history(oldest_first = False, limit = 10000).flatten()
+    messages = []
+    for message in rawmessages:
+        if(text=="" and message.author==ctx.author and "~quoteme" not in message.content):messages.append(message)
+        elif(text!="" and message.author==ctx.author and text in message.content and "~quoteme" not in message.content):messages.append(message)
+    if(len(messages)==0): await ctx.send("no message was found with your specifications!")
+    else:
+        cont = True
+        count = 0
+        while(cont):
+            rand = random.randint(0,len(messages)-1)
+            message = messages[rand]
+            if(message.content.split() or count > 10): cont = False
+            count += 1
+        await ctx.send(ctx.message.author.name + ", on "+str(message.created_at.replace(microsecond=0))+", you said \"" + message.content + "\"")
+    await cmdlog('quoteme')
+#=======================================#
+@client.command()
+async def quote(ctx,user: discord.User,*,text = ""):
+    messages = await ctx.channel.history(oldest_first = False, limit = 100000).flatten()
+    messages2 = []
+    for message in messages:
+        if(text == "" and message.author == user): messages2.append(message)
+        elif(text!="" and message.author == user and text in message.content): messages2.append(message)
+    messages = messages2
+    if(len(messages)>0):
+        rand = random.randint(0,len(messages)-1)
+        message = messages[rand]
+        await ctx.send("on "+str(message.created_at.replace(microsecond=0))+", "+user.name+" said \"" + message.content + "\"")
+    else: await ctx.send("no message was found with your specifications!")
+    await cmdlog("quote")
+#=======================================#
+@client.command()
+async def quoteall(ctx,user: discord.User,*,text = ""):
+    await ctx.send("on it! this might take a minute, so hold on to your hat(s)!")
+    print("before search")
+    messages = await ctx.channel.history(oldest_first = False, limit = 100000).flatten()
+    print("after search")
+    messages2 = []
+    for message in messages:
+        if(text == "" and message.author == user): messages2.append(message)
+        elif(text!="" and message.author == user and text.lower() in message.content.lower()): messages2.append(message)
+    messages = messages2
+    if(len(messages)) == 0: await ctx.send("no message was found with your specifications!")
+    elif(len(messages)<11):
+        for message in reversed(messages):
+            await ctx.send("on "+str(message.created_at.replace(microsecond=0))+", "+user.name+" said \"" + message.content + "\"")
+    else:
+        basename = "quotes"
+        suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+        filename = "_".join([basename, suffix, ".txt"]) 
+        #filename = ".\\"+str(uuid.uuid4().hex)+".txt"
+        file  = open(filename, "w+", encoding="utf-8") 
+        for message in reversed(messages): file.write("on "+str(message.created_at.replace(microsecond=0))+", "+user.name+" said \"" + message.content + "\"\n")
+        file.close()
+        await ctx.send("here are your quotes! there were too many, so I put them in a file :D",file=discord.File(filename))
+        os.remove(filename)
+    await cmdlog("quote")
 #=======================================#
 @client.command()
 async def rock(ctx):
@@ -380,21 +476,93 @@ async def cmdlog(msg):
         global totalCommands
         totalCommands+=1
         print(msg+' '*((8-len(msg))+1)+str(round(client.latency*1000))+'ms')
+#=======================================# clears the screen
+def cls():
+    if name == 'nt': _ = system('cls')
+    else: _ = system('clear')
 #=======================================#
 async def inputLoop():
+    global channel
+    controlling = False
+    id = 0
     while True:
         inp = await inputAsync()
-        if("control" in inp):
-            id = int(inp[8:])
-            channel = client.get_channel(id)
-            print("now controlling bot in channel "+ str(id))
-            while True:
-                msg = await inputAsync()
-                if msg == "stop":
-                    print("no longer controlling bot in channel "+ str(id))
-                    break
-                await channel.send(msg)
-        if("exit" in inp): sys.exit()
+        cmd = inp.split()
+        if(len(cmd)<1): continue
+        cmdlen = len(cmd)
+
+        if(cmd[0]=="/list" and cmdlen>1):
+                if(cmd[1] == "channels" and len(cmd)>2):
+                    for chan in client.get_guild(int(cmd[2])).channels:
+                        if str(chan.type) == 'text':
+                            print(chan.name + "   "+str(chan.id))
+                elif(cmd[1]=="servers"):
+                    for guild in client.guilds:
+                        print(guild.name + "   "+ str(guild.id))
+                elif(cmd[1] == "channels"):
+                    for guild in client.guilds:
+                        for chan in guild.channels:
+                            if str(chan.type) == 'text':
+                                print(guild.name + "   " + chan.name + "   "+str(chan.id))
+
+        elif(cmd[0]=="/watch" and cmdlen>1):
+            if(cmd[1]=="none"):
+                channel = NULL
+                print("no longer watching channel "+ str(id))
+                if(controlling):
+                    controlling = False
+                    print("no longer controlling client in channel "+ str(id))
+            else:
+                id = cmd[1]
+                channel = client.get_channel(id)
+                print("now watching channel "+ str(id))
+
+        elif(cmd[0]=="/control"):
+            if(cmdlen>1 and cmd[1]=="none"):
+                controlling = False
+                print("no longer controlling client in channel "+ str(id))
+            elif(cmdlen>1):
+                if(channel): print("no longer watching channel "+ str(id) + "\n" + "now watching channel "+ cmd[1])
+                id = int(cmd[1])
+                channel = client.get_channel(id)
+                controlling = True
+                print("now watching channel "+ str(id))
+                print("now controlling client in channel "+ str(id))
+            elif(channel):
+                controlling = True
+                print("now controlling client in channel "+ str(id))
+
+        elif(inp == "/exit"): sys.exit()
+
+        elif(inp == "/cls"): cls()
+
+        elif(cmd[0]=="/gethistory" and cmdlen>1):
+            historychannel = client.get_channel(int(cmd[1]))
+            limit = 200
+            if(cmdlen>2): limit = int(cmd[2])
+            try: 
+                messages = await historychannel.history(oldest_first = False, limit = 200).flatten()
+                print("\n")
+                for msg in reversed(messages):
+                    timestamp = ""
+                    if(cmdlen>3 and cmd[3]=="true"): timestamp = str(messages[0].created_at.replace(microsecond=0))
+                    links = ""
+                    for attachment in msg.attachments:
+                        links += " ["+attachment.url+"]"
+                    print(timestamp + " [" + msg.author.name + "] " + msg.content + links)
+            except: print("error reading channel history, most likely lacking permissions")
+
+
+        elif(cmd[0] == "/toggle" and cmd[1]=="logs"): 
+            global doCmdlog
+            if(cmdlen<=2): doCmdlog = not doCmdlog
+            else: doCmdlog = cmd[2].lower() in ("yes", "true", "t", "1")
+            print("command logging set to "+ str(doCmdlog))
+
+        elif(channel and controlling): 
+            try: await channel.send(inp)
+            except: print("error sending message, most likely lacking permissions")
+
 #=======================================# opening the token and running the client
 print('connecting...')
 try: client.run(TOKEN)
